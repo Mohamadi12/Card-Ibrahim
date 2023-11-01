ee510d5756df6fe0e2e5fa84fc5089d72c0d7368
Mohamadi12 <adilnana78@gmail.com>
2023-11-02 00:03:53 +0100

    first commit

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <img className="card-image" src='https://upload.wikimedia.org/wikipedia/commons/d/d7/Ibrahim_Traor%C3%A9_-_2023_%28cropped%29.png' alt="photo de profil"></img>
+      <h2 className="card-title">Africa Nana</h2>
+      <p className="card-text">I get along with my Sister & my Brother</p>
+    </div>
+  )
+}
+
+export default Card
